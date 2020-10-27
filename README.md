525d9f307eb39768938b1932c9b66896d1a5c2d5
Alex Olar <olaralex666@gmail.com>
2020-10-27 17:36:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## Secret Christmas angel
+
+It is an issue for us all to automate the Christmas angel this year due to COVID-19. This application let's you add the names of your family members with whom you wish to exchange gifts and sends out automated emails with the name of a random other family member.
+
+## Usage
+
+Set-up an app password fro Gmail if you have two-step authentication enabled outherwise just add your password in a `config.json` file:
+
+```json
+{
+  "username": "<your-gmail-user-name>",
+  "password": "<your-app-password-or-gmail-password>",
+  "recipients": [
+    {
+      "mail": "recipient@1.com",
+      "name": "recipient1" // This name will be used in the mail!
+    },
+    // ...
+    {
+      "mail": "recipient@2.com",
+      "name": "recipient2"
+    }
+  ]
+}
+```
+
+## Dependencies
+
+`yagmail` and you should install the pdf renderer. Currently the setup only works on Linux.
+
+```bash
+pipenv install
+sudo apt-get install wkhtmltopdf
+```
+
+## Usage
+
+If you have everything setup you should just choose the language and send out your emails to your loved ones.
+
+## @Regards, Alex
---
+++ christmas_angel.py
@@ -0,0 +1,78 @@
+import random
+import json
+import yagmail
+import os
+import argparse
+
+random.seed(42)
+
+language_defaults = {
+    "en": {
+        "subject": "Christmas Greetings!",
+        "contents": "You should find you loved one on the card! Buy a nice gift! :)",
+        "encoding": "utf-8"
+    },
+    "hu": {
+        "subject": "karácsonyi angyal TESZT",
+        "contents": "EZ EGY TESZ ÜZENET! :)",
+        "encoding": "iso-8859-2"
+    }
+}
+
+
+def run(args):
+    # https://stackoverflow.com/questions/25200220/generate-a-random-derangement-of-a-list
+    def random_derangement(n):
+        while True:
+            v = [i for i in range(n)]
+            for j in range(n - 1, -1, -1):
+                p = random.randint(0, j)
+                if v[p] == j:
+                    break
+                else:
+                    v[j], v[p] = v[p], v[j]
+            else:
+                if v[0] != 0:
+                    return tuple(v)
+
+    config = json.load(open('config.json', 'r'))
+    recipients = config['recipients']
+    n_participants = len(recipients)
+
+    derangement = random_derangement(n_participants)
+
+    yag = yagmail.SMTP(config['username'], config['password'])
+    yag.set_logging(yagmail.logging.INFO)
+
+    html = open(f"card_template_{args.language}.html", "r").read()
+
+    for ind, rec in enumerate(derangement):
+        angel_mail = recipients[ind]["mail"]
+        angel_name = recipients[ind]["name"]
+
+        recipient_name = recipients[rec]["name"]
+        recipient_name = "A VIRGÁCSOK"
+
+        message = html.format(angel_name, recipient_name)
+
+        open('template.html', 'w',
+             encoding=language_defaults[args.language]["encoding"]).write(message)
+        os.system(
+            f'wkhtmltopdf --encoding {language_defaults[args.language]["encoding"]} template.html out.pdf')
+
+        yag.send(to=angel_mail, subject=language_defaults[args.language]["subject"],
+                 contents=language_defaults[args.language]["contents"],
+                 attachments=['./out.pdf'])
+
+        os.system('rm template.html')
+        os.system('rm out.pdf')
+
+        break
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--language', default="en", type=str, choices=["en", "hu"])
+
+args = parser.parse_args()
+
+run(args=args)
